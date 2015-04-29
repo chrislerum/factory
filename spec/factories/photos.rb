4b7bc052bae952a3f29865b9a391f72cd0a59b23
Chris Lerum <chris.lerum@gmail.com>
2015-04-29 11:30:11 -0400

    working spec

---
+++ spec/factories/photos.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :photo, class: "Photo" do
+
+    factory :photo_one do
+    end
+
+    factory :photo_two do
+    end
+
+    factory :photo_three do
+    end
+
+    factory :photo_with_gallery do
+      gallery
+    end
+  end
+end
